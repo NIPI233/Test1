5bba9f64659a8a3180123f072d81f7644f5012e6
NIPI233 <2392959673@qq.com>
2020-10-12 14:15:04 +0800

    new

--- src/test/test.java
+++ src/test/test.java
@@ -6,6 +6,7 @@ import java.util.List;
 
 public class test {
 	public static void main(String[] args) {
+		//¸üÐÂ
 //		String str = "Group.@full_name.@Product Engineering.Engineering.JARC";
 //		String[] s = str.split("\\.@");
 //		for(String s1:s) {
