e0f9ae017d3f7f1ffc04f4dd9c050ce97e361818
NIPI233 <2392959673@qq.com>
2020-10-12 12:36:11 +0800

    2020_10_test

---
+++ src/test/test.java
@@ -0,0 +1,33 @@
+package test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class test {
+	public static void main(String[] args) {
+//		String str = "Group.@full_name.@Product Engineering.Engineering.JARC";
+//		String[] s = str.split("\\.@");
+//		for(String s1:s) {
+//			System.out.println(s1);
+//		}
+		
+		//List<String[]> xml3Value = new ArrayList<String[]>();
+		String str = "";
+		//List<String> result = Arrays.asList(str.split(","));
+		//xml3Value="";
+		
+		if(str=="") {
+			System.out.println(str);
+		}
+		else {
+		System.out.println("111");
+		}
+//		for(String i:result) {
+//			System.out.println(i);
+//		}
+		
+		
+	}
+
+}
